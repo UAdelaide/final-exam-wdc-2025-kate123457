32f5ce844a068e101b0d521c9207ae6e156484a0
kate123457 <wenqinfei@gmail.com>
2025-06-20 08:45:27 +0000

    [automatic commit] 2025-06-20 08:45:26 UTC 119.15.75.32

---
+++ part1/db.js
@@ -0,0 +1,15 @@
+// part1/db.js
+const mysql = require('mysql2');
+const pool = mysql
+  .createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '12345',            // 你的 root 密码
+    database: 'DogWalkService',
+    multipleStatements: true, // 允许一次执行多语句
+    waitForConnections: true,
+    connectionLimit: 10,
+  })
+  .promise();
+
+module.exports = pool;
