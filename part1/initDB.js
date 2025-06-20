38e0e3083d6f604b0f61273de18add13133c3375
kate123457 <wenqinfei@gmail.com>
2025-06-20 09:46:00 +0000

    [automatic commit] 2025-06-20 09:46:00 UTC 119.15.75.32

--- part1/initDB.js
+++ part1/initDB.js
@@ -4,11 +4,11 @@ const path = require('path');
 const pool = require('./db');
 
 async function init() {
-  // 读取并执行建表脚本
+
   const ddl = fs.readFileSync(path.join(__dirname, 'dogwalks.sql'), 'utf8');
   await pool.query(ddl);
 
-  // 读取并执行插入示例数据脚本
+
   const dml = fs.readFileSync(path.join(__dirname, 'insert_data.sql'), 'utf8');
   await pool.query(dml);
 }
