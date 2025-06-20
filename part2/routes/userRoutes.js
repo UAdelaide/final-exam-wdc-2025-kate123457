6f19981262c7b0eb3044435fc4f8c89f74c9f2a8
kate123457 <wenqinfei@gmail.com>
2025-06-20 15:31:48 +0000

    [automatic commit] 2025-06-20 15:31:48 UTC 119.15.75.32

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -79,7 +79,7 @@ router.get('/dogs', async (req, res) => {
     res.status(500).json({ error: 'Failed to fetch dogs' });
   }
 });
-
+   //dog api
 router.get('/all-dogs', async (req, res) => {
   const [rows] = await db.query('SELECT dog_id, owner_id, name, size FROM Dogs');
   res.json(rows);
